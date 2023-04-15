5d5931141b7ea69d5325848b4137cb236a666826
John kim <john@bitski.com>
2023-04-14 21:06:18 -0700

    free button routes correctly

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  api: {
+    responseLimit: '8mb',
+  },
+
 }
 
 module.exports = nextConfig
--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -3,32 +3,51 @@ import scrapeGithubRepo from "../../utils/scrapeGithubRepo";
 import { loadEnvConfig } from "@next/env";
 loadEnvConfig("");
 
+
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  const { repoUrl, page, perPage } = req.query;
-
-  if (typeof repoUrl !== "string") {
-    return res.status(400).json({ error: "Invalid repoUrl parameter" });
-  }
-
-  const currentPage = parseInt(page as string) || 1;
-  const itemsPerPage = parseInt(perPage as string) || 10;
-
-  try {
-    const scrapedText = await scrapeGithubRepo(repoUrl, process.env.GITHUB_TOKEN);
-
-    const paginatedData = Array.from(scrapedText)
-      .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
-
-    res.status(200).json({
-      data: paginatedData,
-      currentPage,
-      totalPages: Math.ceil(scrapedText.size / itemsPerPage),
-    });
-  } catch (error) {
-    const err = error as Error & { code?: string, message?: string }; // Assert the type of error
-    res.status(500).json({ error: err.message });
-  }
+    try {
+        const repoUrl = req.query.repoUrl as string;
+        if (typeof repoUrl !== "string") {
+            return res.status(400).json({ error: "Invalid repoUrl parameter" });
+        }
+        const scrapedText = await scrapeGithubRepo(repoUrl, process.env.GITHUB_TOKEN);
+        res.status(200).json({ data: Array.from(scrapedText.entries()) });
+    } catch (error) {
+        const err = error as Error & { code?: string, message?: string }; // Assert the type of error
+        res.status(400).json({ error: err.message });
+    }
 }
+
+
+// export default async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse
+// ) {
+//     const { repoUrl, page, perPage } = req.query;
+
+//     if (typeof repoUrl !== "string") {
+//         return res.status(400).json({ error: "Invalid repoUrl parameter" });
+//     }
+
+//     const currentPage = parseInt(page as string) || 1;
+//     const itemsPerPage = parseInt(perPage as string) || 10;
+
+//     try {
+//         const scrapedText = await scrapeGithubRepo(repoUrl, process.env.GITHUB_TOKEN);
+
+//         const paginatedData = Array.from(scrapedText)
+//             .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+//         res.status(200).json({
+//             data: paginatedData,
+//             currentPage,
+//             totalPages: Math.ceil(scrapedText.size / itemsPerPage),
+//         });
+//     } catch (error) {
+//         const err = error as Error & { code?: string, message?: string }; // Assert the type of error
+//         res.status(500).json({ error: err.message });
+//     }
+// }
